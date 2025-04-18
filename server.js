c7c81fa494f50bfc601e3f9f4777535ba2030ebe
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-04-19 07:09:05 +0800

    070919

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+const PORT=6003;
+app.use(express.static(__dirname));
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+});
